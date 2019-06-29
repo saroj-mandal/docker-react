e5edebfd4d6271825ee8054de4fe125a16a61b54
Saroj Mandal <saroj.mandal1989@gmail.com>
2019-06-29 22:18:14 +0530

    modified code

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello there, this is cool bro!
+          Hello there, this is cool bro! I am trying to upload this to aws
         </p>
         <a
           className="App-link"
